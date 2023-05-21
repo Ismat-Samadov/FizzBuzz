f1d002cb7db47f232bddb94a13f3a7a79af30ad0
Səmədov İsmət <ismetsemedov@gmail.com>
2023-05-21 17:18:34 +0400

    FizzBuzz for Python

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,9 +3,7 @@ public class FizzBuzz {
     public static void main(String[] args) {
         FizzBuz(1, 100, 4, 6);
     }
-
     public static void FizzBuz(int lowerBound, int upperBound, int FirstPattern, int SecondPattern) {
-
         for (int i = lowerBound; i <= upperBound; i++) {
             if ((i % FirstPattern == 0 && i % SecondPattern == 0)) {
                 System.out.println("FizzBuzz");
